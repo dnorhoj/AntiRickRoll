48ecf6a8660f5631a87e8efe06de1fbb5bfb6eca
dnorhoj <daniel.norhoj@gmail.com>
2021-07-26 16:33:38 +0200

    Removed TheBlock (plans for rickrolldb.me), and updated to make css compatible with dark mode

--- AntiRickRoll/content.js
+++ AntiRickRoll/content.js
@@ -1,13 +1,21 @@
-chrome.storage.local.get('bypassed', async result => {
-    if (result.bypassed) {
-        chrome.storage.local.set({"bypassed": false});
-    } else {
-        const res = await fetch(`${location.protocol}//antirickroll.theblock.heliohost.org/api?url=${location.href}&extension=1`)
-            .then(data => { return data.json() })
-            .catch(e => { console.error(`Error checking for Rick Roll: ${e}`) });
-        console.log(res);
-        if (res.status) {
-            location = chrome.runtime.getURL("warn/warn.html")+"?"+location.href;
-        }
+(() => {
+    let blocked_ids = [
+        "dQw4w9WgXcQ",
+        "-51AfyMqnpI",
+        "oHg5SJYRHA0",
+        "cvh0nX08nRw",
+        "V-_O7nl0Ii0",
+        "2ocykBzWDiM"
+    ];
+
+    if(blocked_ids.find(i => location.href.includes(i))) {
+        chrome.storage.local.get('bypassed', result => {
+            console.log(result)
+            if(result['bypassed'] !== true) {
+                location = chrome.runtime.getURL("warn/warn.html")+"?"+location.href;
+            } else {
+                chrome.storage.local.set({"bypassed": false})
+            }
+        });
     }
-});
+})();
