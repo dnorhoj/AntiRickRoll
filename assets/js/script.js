028973e71da742e4f96b776b971b82dbcb9b0a5a
dnorhoj <daniel.norhoj@gmail.com>
2021-09-28 15:21:13 +0200

    Testing

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+window.onload = () => {
+    let switcher = document.querySelector('#switcher');
+    let screenshot_light = document.querySelector('#screenshot1');
+    let screenshot_dark = document.querySelector('#screenshot2');
+
+    switcher.addEventListener('click', () => {
+        if (DarkReader.isEnabled()) {
+            screenshot_light.style.display = 'block';
+            screenshot_dark.style.display = 'none';
+            DarkReader.disable();
+        } else {
+            screenshot_light.style.display = 'none';
+            screenshot_dark.style.display = 'block';
+            DarkReader.enable();
+        }
+    });
+};
