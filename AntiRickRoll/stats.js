4df1e0f951211094da80280fc0c3e8dbd1c63e67
Hayden <57072675+Qeolvdxu@users.noreply.github.com>
2021-10-14 12:14:18 -0500

    Added variables

--- AntiRickRoll/stats.js
+++ AntiRickRoll/stats.js
@@ -1,4 +1,14 @@
 function toggleBlocking() {
 }
 
+window.onload = function(){
+    var total;
+    var unique;
+
+    // Update total and unique variable numbers here
+  
+    document.getElementById('total').innerHTML = total;
+    document.getElementById('unique').innerHTML = unique;
+};
+
 document.querySelector("form").addEventListener("toggle", toggleBlocking);
