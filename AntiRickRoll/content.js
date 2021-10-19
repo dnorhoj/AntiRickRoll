f022d838293086389c04d7cdbfdbe888fb9a8c13
luis-1063 <52985915+luis-1063@users.noreply.github.com>
2021-10-18 21:39:05 -0500

    display data on icon and can count total

--- AntiRickRoll/content.js
+++ AntiRickRoll/content.js
@@ -1,4 +1,5 @@
-(() => {
+(() => 
+{
     let enabled = true;
     let blocked_ids = [
         "dQw4w9WgXcQ",
@@ -10,13 +11,44 @@
         "j5a0jTc9S10",
         "HPk-VhRjNI8"
     ];
+    var length = 8;
 
-    if(enabled && blocked_ids.find(i => location.href.includes(i))) {
-        chrome.storage.local.get('bypassed', result => {
+    if(enabled && blocked_ids.find(i => location.href.includes(i))) 
+    {
+        chrome.storage.local.get('total', result =>
+        {
+        if (result['total'] === null)
+        {
+            chrome.storage.local.set({"total": 0})
+            chrome.storage.local.set({"idArr": [false,false,false,false,false,false,true,true]})
+        }
+
+            chrome.storage.local.set({"total": result['total'] + 1})
+        });
+
+        chrome.storage.local.get('idArr', result =>
+        {
+            let tempArray = [false,false,false,false,false,false,false,false];
+
+            if (result['idArr'] !== null)
+            {
+                tempArray = result['idArr'];
+            }
+
+            tempArray[blocked_ids.findIndex(i => location.href.includes(i))] = true;
+
+            chrome.storage.local.set({"idArr": tempArray})
+        });
+
+        chrome.storage.local.get('bypassed', result => 
+        {
             console.log(result)
-            if(result['bypassed'] !== true) {
+            if(result['bypassed'] !== true) 
+            {
                 location = chrome.runtime.getURL("warn/warn.html")+"?"+location.href;
-            } else {
+            } 
+            else 
+            {
                 chrome.storage.local.set({"bypassed": false})
             }
         });
