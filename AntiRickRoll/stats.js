c0841bcc9dbc67d0a7aeb980dcb36a41d3b3cc9d
luis-1063 <52985915+luis-1063@users.noreply.github.com>
2021-10-18 21:39:54 -0500

    stats can show the total

--- AntiRickRoll/stats.js
+++ AntiRickRoll/stats.js
@@ -1,18 +1,40 @@
+
 function toggleBlocking() {
-    if (enabled == true)
+    if (enabled === true)
         enabled = false;
     else
         enabled = true;
 }
 
-window.onload = function(){
-    var total;
-    var unique;
+window.onload = function()
+{
 
-    // Update total and unique variable numbers here
-  
-    document.getElementById('total').innerHTML = total;
-    document.getElementById('unique').innerHTML = unique;
+	var total = 0;
+	var unique = 0;
+	var tempArr = [false,false,false,false,false,false,false,false];
+
+	// Update total and unique variable numbers here
+
+
+	chrome.storage.local.get('total', result =>
+	{
+		total = result['total'] || total;
+		document.getElementById('total').innerHTML = total
+		chrome.storage.local.set({'total': total})
+	});
+
+	chrome.storage.local.get('idArr', result =>
+	{
+		tempArr = result['idArr'] || tempArr;
+		for (let i = 0; i < tempArr.length; i += 1)
+		{
+			if(tempArr[i] === true)
+			{
+				unique += 1;
+			}
+		}
+		document.getElementById('unique').innerHTML = unique;
+	});
 };
 
-document.querySelector("form").addEventListener("toggle", toggleBlocking);
+//document.querySelector("form").addEventListener("checkbox", toggleBlocking);
