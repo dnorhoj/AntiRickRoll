82f5da22402ac590818815ed049da3e6da2e884b
luis-1063 <52985915+luis-1063@users.noreply.github.com>
2021-10-19 20:29:55 -0500

    popup can read the stats

--- AntiRickRoll/popup/popup.js
+++ AntiRickRoll/popup/popup.js
@@ -14,3 +14,38 @@
 		});
 	})
 })();
+
+window.onload = function()
+{
+
+	var total = 0;
+	var unique = 0;
+	var tempArr = [false,false,false,false,false,false,false,false];
+
+	// Update total and unique variable numbers here
+
+
+	chrome.storage.local.get('total', result =>
+	{
+		total = result['total'] || total;
+		document.getElementById('total').innerHTML = total
+		chrome.storage.local.set({'total': total})
+	});
+
+	chrome.storage.local.get('idArr', result =>
+	{
+		tempArr = result['idArr'] || tempArr;
+		for (let i = 0; i < tempArr.length; i += 1)
+		{
+			if(tempArr[i] === true)
+			{
+				unique += 1;
+			}
+		}
+		if (total === 0)
+		{
+			unique = 0;
+		}
+		document.getElementById('unique').innerHTML = unique;
+	});
+};
