2814193ed52c3651c9c3bd9ce8a6a8bbc7b854fa
dnorhoj <daniel.norhoj@gmail.com>
2021-11-29 22:17:20 +0100

    First beta of popup

--- AntiRickRoll/popup/popup.js
+++ AntiRickRoll/popup/popup.js
@@ -1,1 +1,17 @@
-// TODO
+(() => {
+	// Get elements
+	let toggler = document.querySelector('#enableToggle');
+    console.log(toggler);
+    console.log("toggler");
+    toggler.checked = true;
+
+	// Set toggler's initial value
+	chrome.storage.local.get('disabled', res => {
+		toggler.checked = !res.disabled;
+	});
+
+	// Give toggler functionality
+	toggler.addEventListener('click', () => {
+        chrome.storage.local.set({"disabled": !toggler.checked});
+	});
+})();
