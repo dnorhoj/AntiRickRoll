5440dbf2ecee546403b1c783a9c929cfe8378c66
Daniel Norhøj <daniel@dnorhoj.me>
2022-11-18 00:05:53 +0100

    First working prototype of svelte popup

---
+++ popup-src/src/App.svelte
@@ -0,0 +1,29 @@
+<script>
+    let counter = null;
+
+    chrome.storage.local
+        .get("counter")
+        .then((data) => (counter = data.counter ?? 0));
+    
+    const increment = () => {
+        counter++;
+        chrome.storage.local.set({ counter });
+    };
+</script>
+
+<div class="p-5">
+    <h1>Svelte test</h1>
+
+    {#if counter === null}
+        <h1>Loading...</h1>
+    {:else}
+        <button
+            class="p-2 bg-blue-500 text-white rounded-md"
+            on:click={increment}
+        >
+            Increment
+        </button>
+
+        <p class="mt-2">Counter: {counter}</p>
+    {/if}
+</div>
---
+++ popup-src/tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.svelte'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
